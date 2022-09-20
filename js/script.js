f0c8cc28513ba29040c84c9100848074f63c2d0f
Nikita Zinyakov <nikuza.genesis@yandex.ru>
2022-09-20 18:07:20 +0300

    автоматизация процессов

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
-
-
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
-    lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    filmRate = prompt('На сколько оцените его?', ''),
-    lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
-    filmRate2 = prompt('На сколько оцените его?', '');
-
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -15,6 +8,23 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-personalMovieDB.movies[lastFilm] = filmRate;
-personalMovieDB.movies[lastFilm2] = filmRate2;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Какой фильм вы смотрели в последний раз', ''),
+        b = prompt('Какую оценку дадите просмотренному фильму', '');
+    if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' & b != '') {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+if (personalMovieDB.count < 10) {
+    console.log('Просмотров довольно мало');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Не могу разобраться.Попробуйте еще');
+}
+
 console.log(personalMovieDB);
